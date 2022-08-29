5e4da516f976738cf08fc65c902079ace200cf42
Miguel Oliveira <deoliveiramiguelarcanjo@gmail.com>
2022-08-29 05:04:49 +0200

    IaC Apache v1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project #2: Infraestrutura como código: Script de provisionamento de umservidor web (Apache) no Linux
+
+# Descrição do projecto
+* Restaurar o snapshot criado anteriormente no VirtualBox;
+* Actualizar o servidor;
+* Instalar o unzip;
+* Baixar a aplicação disponível no endereço  no directório <b>/tmp</b>;
+* Copiar os arquivos da aplicação no directório padrão do apache;
+* Fazer o upload do arquivo script para um repositório no GitHub.
---
+++ script-iac-proj-2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# update linux server
+apt-get update && apt-get upgrade -y
+
+# install apache2
+apt-get install apache2 -y
+
+# install unzip
+apt-get install unzip -y
+
+# goto /tmp directory
+cd /tmp
+
+# download the web app on /tmp directory
+wget
+
+# unzip the web app
+unzip main.zip
+
+# change to linux-site-dio-main directory and copy recursively all application files to apache default directory
+cd linux-site-dio-main 
+cp -R * /var/www/html/
